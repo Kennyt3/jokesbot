0ae716f6d8e10621510ad12757d5f5bf70536f18
Taiwo Mercy <kennyt341@gmail.com>
2022-12-04 21:59:47 +0100

    built a robot joke telling app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/home'
 
 function App() {
   return (
-    <div style={{ backgroundColor: 'rgb(100, 194, 245)' }}>
+    <div style={{ backgroundColor: 'rgb(100, 194, 245)', height: '100%' }}>
       <Home />
     </div>
   )
