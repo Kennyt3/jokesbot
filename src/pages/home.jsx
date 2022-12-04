7b2d4f0553a6d2646a4ab28b57bb23acfc9f6dc2
Taiwo Mercy <kennyt341@gmail.com>
2022-12-04 21:35:31 +0100

    built a robot joke telling app

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,11 +4,13 @@ import Title from '../Component/Title'
 const Home = () => {
   return (
     <div
-      style={{
-        backgroundColor: 'rgb(100, 194, 245)',
-        // height: '100vh',
-        // width: '100vw',
-      }}
+      style={
+        {
+          // backgroundColor: 'rgb(100, 194, 245)',
+          // height: '100vh',
+          // width: '100vw',
+        }
+      }
     >
       <div
         style={{
